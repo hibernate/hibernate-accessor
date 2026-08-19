8ce8f4794592f035cc6f6de166dcdd14b3dc321f
marko-bekhta <marko.prykladna@gmail.com>
2026-08-19 17:08:16 +0200

    Integrate the reflection fallback into other factories
    Co-authored-by: Andrea Boriero <andrea@hibernate.org>

--- hibernate-accessor-asm/src/main/java/module-info.java
+++ hibernate-accessor-asm/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.hibernate.accessor.asm {
 	requires org.hibernate.accessor;
 	requires org.objectweb.asm;
+	requires static org.jboss.logging;
 
 	exports org.hibernate.accessor.asm;
 	exports org.hibernate.accessor.asm.spi;
--- hibernate-accessor-bytebuddy/src/main/java/module-info.java
+++ hibernate-accessor-bytebuddy/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.hibernate.accessor.bytebuddy {
 	requires org.hibernate.accessor;
 	requires net.bytebuddy;
+	requires static org.jboss.logging;
 
 	exports org.hibernate.accessor.bytebuddy;
 	exports org.hibernate.accessor.bytebuddy.spi;
