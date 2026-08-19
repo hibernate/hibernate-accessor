20bc9c47fb26d45207b0772d48f12f4dd04c7606
marko-bekhta <marko.prykladna@gmail.com>
2026-08-19 14:37:40 +0200

    Add multi-value accessor interfaces for bulk field/method access
    Co-authored-by: Andrea Boriero <andrea@hibernate.org>

--- hibernate-accessor/src/main/java/org/hibernate/accessor/lambda/impl/HibernateAccessorLambdaFactory.java
+++ hibernate-accessor/src/main/java/org/hibernate/accessor/lambda/impl/HibernateAccessorLambdaFactory.java
@@ -6,6 +6,8 @@ package org.hibernate.accessor.lambda.impl;
 
 import org.hibernate.accessor.HibernateAccessorFactory;
 import org.hibernate.accessor.HibernateAccessorInstantiator;
+import org.hibernate.accessor.HibernateAccessorMultiValueReader;
+import org.hibernate.accessor.HibernateAccessorMultiValueWriter;
 import org.hibernate.accessor.HibernateAccessorValueReader;
 import org.hibernate.accessor.HibernateAccessorValueWriter;
 import org.hibernate.accessor.logging.impl.CoreLog;
@@ -17,6 +19,7 @@ import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.lang.reflect.Member;
 import java.lang.reflect.Method;
 
 public class HibernateAccessorLambdaFactory implements HibernateAccessorFactory {
@@ -109,5 +112,41 @@ public class HibernateAccessorLambdaFactory implements HibernateAccessorFactory 
 			}
 			throw CoreLog.INSTANCE.errorCreatingHandle(setter, t, t.getMessage());
 		}
+	}
+
+	@Override
+	public HibernateAccessorMultiValueReader multiValueReader(Member... members) {
+		final HibernateAccessorValueReader<?>[] readers = new HibernateAccessorValueReader<?>[members.length];
+		for ( int i = 0; i < members.length; i++ ) {
+			final Member member = members[i];
+			if ( member instanceof Field field ) {
+				readers[i] = valueReader( field );
+			}
+			else if ( member instanceof Method method ) {
+				readers[i] = valueReader( method );
+			}
+			else {
+				throw new IllegalArgumentException( "Unsupported member type: " + member.getClass().getName() );
+			}
+		}
+		return new LambdaMultiValueReader( readers );
+	}
+
+	@Override
+	public HibernateAccessorMultiValueWriter multiValueWriter(Member... members) {
+		final HibernateAccessorValueWriter[] writers = new HibernateAccessorValueWriter[members.length];
+		for ( int i = 0; i < members.length; i++ ) {
+			final Member member = members[i];
+			if ( member instanceof Field field ) {
+				writers[i] = valueWriter( field );
+			}
+			else if ( member instanceof Method method ) {
+				writers[i] = valueWriter( method );
+			}
+			else {
+				throw new IllegalArgumentException( "Unsupported member type: " + member.getClass().getName() );
+			}
+		}
+		return new LambdaMultiValueWriter( writers );
 	}
 }
