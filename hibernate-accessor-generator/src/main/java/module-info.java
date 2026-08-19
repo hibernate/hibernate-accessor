d0909401c659ff3a33bcf1cc8906f8825f0e0819
marko-bekhta <marko.prykladna@gmail.com>
2026-08-19 15:54:41 +0200

    Add hibernate-models-accessor-generator module for build-time accessor bytecode generation
    Co-authored-by: Andrea Boriero <andrea@hibernate.org>

---
+++ hibernate-accessor-generator/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module org.hibernate.accessor.generator {
+	requires org.hibernate.accessor;
+	requires org.objectweb.asm;
+
+	exports org.hibernate.accessor.generator;
+	exports org.hibernate.accessor.generator.runtime;
+}
