b978dab4183048fbe6dbd1f412b355f098524ac1
marko-bekhta <marko.prykladna@gmail.com>
2026-08-19 17:15:38 +0200

    Drop build-time generator (at least for now)
    Co-authored-by: Andrea Boriero <andrea@hibernate.org>

--- hibernate-accessor-generator/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module org.hibernate.accessor.generator {
-	requires org.hibernate.accessor;
-	requires org.objectweb.asm;
-
-	exports org.hibernate.accessor.generator;
-	exports org.hibernate.accessor.generator.runtime;
-}
