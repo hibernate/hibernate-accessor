9e117c607dec1b89461d0bf6e4124a622197b232
marko-bekhta <marko.prykladna@gmail.com>
2026-08-24 18:42:08 +0200

    Wire method-handle factory into HibernateAccessorFactory

--- testing/hibernate-accessor-tck-methodhandle/src/test/java/org/hibernate/accessor/tck/methodhandle/MethodhandleTckAccessorConfiguration.java
+++ testing/hibernate-accessor-tck-methodhandle/src/test/java/org/hibernate/accessor/tck/methodhandle/MethodhandleTckAccessorConfiguration.java
@@ -1,7 +1,6 @@
 package org.hibernate.accessor.tck.methodhandle;
 
 import org.hibernate.accessor.HibernateAccessorFactory;
-import org.hibernate.accessor.methodhandle.impl.HibernateAccessorMethodHandleFactory;
 import org.hibernate.accessor.tck.util.TckAccessorConfiguration;
 
 import java.lang.invoke.MethodHandles;
@@ -9,7 +8,7 @@ import java.lang.invoke.MethodHandles;
 public class MethodhandleTckAccessorConfiguration implements TckAccessorConfiguration {
     @Override
     public HibernateAccessorFactory factory() {
-        return new HibernateAccessorMethodHandleFactory(MethodHandles.lookup());
+        return HibernateAccessorFactory.methodHandle(MethodHandles.lookup());
     }
 
 }
